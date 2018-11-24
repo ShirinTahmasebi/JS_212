4715e201f31db618bc0c61ff7dc2b0002a250580
ShirinTahmasebi <shirin_tahmasebi94@yahoo.com>
2018-11-24 03:36:43 +0330

    Don't break naming convention rules!

--- controller/v1/questions/questions.js
+++ controller/v1/questions/questions.js
@@ -5,26 +5,30 @@ const to = require("../../../utils/to").to;
 const append_error_and_call_next = require("../../../utils/to").append_error_and_call_next;
 
 module.exports.get_single_question = async (req, res, next) => {
-  const [get_question_query, get_choices_query] = await this.getQueryByQuestionType(req.query.question_type);
+  const [get_question_query, get_choices_query] = await this.get_query_by_question_type(req.query.question_type);
   const [get_question_error, get_question_result] = await to(execute_query(get_question_query));
   if (get_question_error) {
     append_error_and_call_next(res, database_errors.CODE_100001, next);
     return;
   }
+
   res.data = await(get_question_result[0] || {});
 
-  const [get_choices_error, get_choices_result] = await to(execute_query(get_choices_query.replace('{question_id}', res.data.question_id)));
-  if (get_choices_error) {
-    append_error_and_call_next(res, database_errors.CODE_100003, next);
-    return;
+  if (get_choices_query !== '') {
+    const [get_choices_error, get_choices_result] = await to(execute_query(get_choices_query.replace('{question_id}', res.data.question_id)));
+    if (get_choices_error) {
+      append_error_and_call_next(res, database_errors.CODE_100003, next);
+      return;
+    }
+    res.data.choices = get_choices_result;
   }
-  res.data.choices = get_choices_result;
+
   // TODO: Store last question id in Redis
   // TODO: Add last question id to user's question ids in Redis
   next();
 };
 
-this.getQueryByQuestionType = async (question_type) => {
+this.get_query_by_question_type = async (question_type) => {
   const QUESTION_TYPES = require('../../../model/question_types');
   if (!question_type) return mysql_queries.questions.get_random_question;
   question_type = parseInt(question_type) || 0;
